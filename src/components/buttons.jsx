a4a6f5759360d145a9cd38830d06456bd87b7e82
HazPD <christianpanen@gmail.com>
2024-08-19 00:07:40 +0800

    added prevent form submission

--- src/components/buttons.jsx
+++ src/components/buttons.jsx
@@ -3,13 +3,20 @@ import React from "react";
 function FormButtons({ viewForm, setviewForm }) {
     return (
         <div className="form-buttons">
-            <button onClick={() => setviewForm('signup')} className={viewForm === 'signup'}>
+            <button 
+                onClick={() => setviewForm('signup')} 
+                className={viewForm === 'signup' ? 'active' : ''}
+                type="button"  // Prevents form submission
+            >
                 Sign Up
             </button>
-            <button onClick={() => setviewForm('login')} className={viewForm === 'login'}>
+            <button 
+                onClick={() => setviewForm('login')} 
+                className={viewForm === 'login' ? 'active' : ''}
+                type="button"  // Prevents form submission
+            >
                 Login
             </button>   
-            
         </div>
     );
 }
